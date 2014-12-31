b60c29f6adcf1be0cce52610831659cfb090d47f
Lawson Caudill <lawson@caudillclan.us>
2014-12-31 18:15:34 -0500

    Initial version.

---
+++ Naos.Utils.FileJanitor/FileJanitorConsoleHarness.cs
@@ -0,0 +1,118 @@
+﻿namespace Naos.Utils.FileJanitor
+{
+    using System;
+    using System.Linq;
+
+    using CLAP;
+
+    using OBeautifulCode.Libs.Collections;
+
+    public class FileJanitorConsoleHarness
+    {
+        [Verb(Aliases = "Cleanup", Description = "Removes old files")]
+        public static void Cleanup(
+            [Required] [Aliases("")] [Description("The root path to evaluate (must be a directory).")] string rootPath,
+            [Required] [Aliases("")] [Description("The time to retain files (in format dd:hh:mm).")] string retentionWindow,
+            [DefaultValue(true)] [Aliases("")] [Description("Whether or not to evaluate files recursively on the path.")] bool recursive,
+            [DefaultValue(false)] [Aliases("")] [Description("Whether or not to delete directories that are or become empty during cleanup.")] bool
+                deleteEmptyDirectories,
+            [DefaultValue(FileJanitor.DateRetrievalStrategy.LastUpdateDate)] [Aliases("")] [Description("The date retrieval strategy to use.")] FileJanitor.DateRetrievalStrategy dateRetrievalStrategy)
+        {
+            var retentionWindowTimeSpan = FileJanitorConsoleHarness.GetTimeSpanFromDayHourMinuteColonDelimited(retentionWindow);
+
+            Console.WriteLine("PARAMETERS:");
+            Console.WriteLine("                  rootPath: " + rootPath);
+            Console.WriteLine("           retentionWindow: " + retentionWindow);
+            Console.WriteLine(" retentionWindow (IN DAYS): " + retentionWindowTimeSpan.TotalDays);
+            Console.WriteLine("    deleteEmptyDirectories: " + deleteEmptyDirectories);
+            Console.WriteLine("                 recursive: " + recursive);
+            Console.WriteLine("     dateRetrievalStrategy: " + dateRetrievalStrategy);
+            Console.WriteLine(string.Empty);
+
+            FileJanitor.Cleanup(
+                rootPath,
+                retentionWindowTimeSpan,
+                recursive,
+                deleteEmptyDirectories,
+                dateRetrievalStrategy);
+        }
+
+        public static TimeSpan GetTimeSpanFromDayHourMinuteColonDelimited(string textToParse)
+        {
+            var argException = new ArgumentException("Value: " + (textToParse ?? string.Empty) + " isn't a valid time, please use format dd:hh:mm.", textToParse);
+            if (string.IsNullOrEmpty(textToParse))
+            {
+                throw argException;
+            }
+
+            var split = textToParse.Split(':');
+            if (split.Length != 3)
+            {
+                throw argException;
+            }
+
+            var daysRaw = split[0];
+            int days;
+            if (!int.TryParse(daysRaw, out days))
+            {
+                throw argException;
+            }
+
+            var hoursRaw = split[1];
+            int hours;
+            if (!int.TryParse(hoursRaw, out hours))
+            {
+                throw argException;
+            }
+
+            var minutesRaw = split[2];
+            int minutes;
+            if (!int.TryParse(minutesRaw, out minutes))
+            {
+                throw argException;
+            }
+
+            return new TimeSpan(days, hours, minutes, 0);
+        }
+
+        [Empty]
+        [Help(Aliases = "h,?,-h,-help")]
+        [Verb(IsDefault = true)]
+        public static void Help(string help)
+        {
+            Console.WriteLine("   Usage");
+            Console.Write("   -----");
+
+            // strip out the usage info about help, it's confusing
+            help = help.Split(new[] { Environment.NewLine }, StringSplitOptions.None).Skip(3).ToNewLineDelimited();
+            Console.WriteLine(help);
+            Console.WriteLine();
+        }
+
+        [Error]
+        public static void Error(ExceptionContext context)
+        {
+            // change color to red
+            var originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            // parser exception or 
+            if (context.Exception is CommandLineParserException)
+            {
+                Console.WriteLine("I don't understand.  Run the exe with the 'help' command for usage.");
+                Console.WriteLine("   " + context.Exception.Message);
+            }
+            else
+            {
+                Console.WriteLine("Something broke while performing janitorial duties.");
+                Console.WriteLine("   " + context.Exception.Message);
+                Console.WriteLine(string.Empty);
+                Console.WriteLine("   " + context.Exception);
+            }
+
+            // restore color
+            Console.WriteLine();
+            Console.ForegroundColor = originalColor;
+        }
+    }
+}
---
+++ Naos.Utils.FileJanitor/Program.cs
@@ -0,0 +1,87 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Naos LLC">
+//   Copyright 2014 Naos LLC
+// </copyright>
+// <summary>
+//   The program.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Utils.FileJanitor
+{
+    using System;
+
+    using CLAP;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The program entry point.
+        /// </summary>
+        /// <param name="args">Command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            WriteAsciiArt();
+            Parser.Run<FileJanitorConsoleHarness>(args);
+        }
+
+        /// <summary>
+        /// Write ASCII art.
+        /// </summary>
+        private static void WriteAsciiArt()
+        {
+            Console.WriteLine(@"______________________________________________________________________________");
+            Console.WriteLine(@"|                                                                            |");
+            Console.WriteLine(@"|                                  . ...  .                                  |");
+            Console.WriteLine(@"|                                    OI$7~$7.                                |");
+            Console.WriteLine(@"|                                ..8DND. +NDO$.                              |");
+            Console.WriteLine(@"|                                 DD?     . N8O                              |");
+            Console.WriteLine(@"|                                .DN.       .N8                              |");
+            Console.WriteLine(@"|                                ID+.       .~DD                             |");
+            Console.WriteLine(@"|                                 DO         IDO                             |");
+            Console.WriteLine(@"|                                .N8O       .DD                              |");
+            Console.WriteLine(@"|                                  N8OI. ..8DD                               |");
+            Console.WriteLine(@"|                                   .MD888DNZ  .                             |");
+            Console.WriteLine(@"|                                      88?                                   |");
+            Console.WriteLine(@"|                                    . 88?                                   |");
+            Console.WriteLine(@"|                                     $D87NO8 ..          .OZ,.8.            |");
+            Console.WriteLine(@"|                                    O.88?   =DOO. ..  ..$8D:DD.             |");
+            Console.WriteLine(@"|                                    8788?    .   ~7OMNN8OD8DO .             |");
+            Console.WriteLine(@"|                                  ...NOZ$ .   .  . .OZO? ....               |");
+            Console.WriteLine(@"|                                     .MD+=8OOO  ~O7D, .                     |");
+            Console.WriteLine(@"|                                     .NN? .8OOZ7N .                         |");
+            Console.WriteLine(@"|                                      88?7O?8ND .                           |");
+            Console.WriteLine(@"|                                     .OO7N.7                                |");
+            Console.WriteLine(@"|                                  ,O$888Z                                   |");
+            Console.WriteLine(@"|                          .. . 7O7D .888O,.                                 |");
+            Console.WriteLine(@"|                          . OOZN. . =OD.DZ                                  |");
+            Console.WriteLine(@"|             DDD..     .,OZ8= ..   .88   88                                 |");
+            Console.WriteLine(@"|          ..8888888~ 7OOD,..      . 8$   DZ                                 |");
+            Console.WriteLine(@"|          .=O888888DD8 .           :8    88 .                               |");
+            Console.WriteLine(@"|           OOOOD8888888D           Z8    ?N.                                |");
+            Console.WriteLine(@"|           OOOOOOOD88Z888$..       88     8.                                |");
+            Console.WriteLine(@"|          ...OOOOOOOD88888         88    .8~                                |");
+            Console.WriteLine(@"|              .OOOOOOO8DD,         MN    .8I.                               |");
+            Console.WriteLine(@"|                 OOZOOZZ.         .M$     8Z                                |");
+            Console.WriteLine(@"|                .  OZZZZ:.        .8~     8D.                               |");
+            Console.WriteLine(@"|                   . .ZZZ         =O,    .88                                |");
+            Console.WriteLine(@"|                                  =8D     NOO.                              |");
+            Console.WriteLine(@"|                                   M8.     88..                             |");
+            Console.WriteLine(@"|                                   .       .  .                             |");
+            Console.WriteLine(@"|                                                                            |");
+            Console.WriteLine(@"|----------------------------------------------------------------------------|");
+            Console.WriteLine(@"|                                                                            |");
+            Console.WriteLine(@"| ,------.,--.,--.            ,--.                ,--.  ,--.                 |");
+            Console.WriteLine(@"| |  .---'`--'|  | ,---.      |  | ,--,--.,--,--, `--',-'  '-. ,---. ,--.--. |");
+            Console.WriteLine(@"| |  `--, ,--.|  || .-. :,--. |  |' ,-.  ||      \,--.'-.  .-'| .-. ||  .--' |");
+            Console.WriteLine(@"| |  |`   |  ||  |\   --.|  '-'  /\ '-'  ||  ||  ||  |  |  |  ' '-' '|  |    |");
+            Console.WriteLine(@"| `--'    `--'`--' `----' `-----'  `--`--'`--''--'`--'  `--'   `---' `--'    |");
+            Console.WriteLine(@"|                                                                            |");
+            Console.WriteLine(@"|____________________________________________________________________________|");
+            Console.WriteLine();
+        }
+    }
+}
