4ba065507322ed23e6ea8a2aaec38abf19ac3f37
Suraj Gupta <SurajGupta@users.noreply.github.com>
2025-02-13 17:43:26 -0500

    added MultipleKeysFoundStrategy to types to register in serialization configuration

--- Naos.FileJanitor.Serialization.Bson/FileJanitorBsonSerializationConfiguration.cs
+++ Naos.FileJanitor.Serialization.Bson/FileJanitorBsonSerializationConfiguration.cs
@@ -8,20 +8,21 @@ namespace Naos.FileJanitor.Serialization.Bson
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
     using Naos.FileJanitor.Domain;
     using OBeautifulCode.Serialization.Bson;
 
     /// <summary>
     /// Implementation for the <see cref="FileJanitor" /> domain.
     /// </summary>
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "FileJanitor", Justification = "Spelling/name is correct.")]
+    [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "FileJanitor", Justification = "Spelling/name is correct.")]
     public class FileJanitorBsonSerializationConfiguration : BsonSerializationConfigurationBase
     {
         /// <inheritdoc />
         protected override IReadOnlyCollection<string> TypeToRegisterNamespacePrefixFilters => new[]
-                                                                                               {
-                                                                                                   typeof(ArchivedDirectory).Namespace,
-                                                                                               };
+        {
+            typeof(ArchivedDirectory).Namespace,
+        };
 
         /// <inheritdoc />
         protected override IReadOnlyCollection<TypeToRegisterForBson> TypesToRegisterForBson => new[]
@@ -29,6 +30,7 @@ namespace Naos.FileJanitor.Serialization.Bson
             typeof(ArchivedDirectory).ToTypeToRegisterForBson(),
             typeof(FileLocation).ToTypeToRegisterForBson(),
             typeof(MetadataItem).ToTypeToRegisterForBson(),
+            typeof(MultipleKeysFoundStrategy).ToTypeToRegisterForBson(),
         };
     }
 }
